9e90d158aab2915809b6e9bbbdf6da816ce86e5f
JamaykonCZ <jorge.mombach@gmail.com>
2023-08-08 13:52:28 -0300

    Added Repo, entity

--- src/main/java/jorge/mombach/school/entity/Student.java
+++ src/main/java/jorge/mombach/school/entity/Student.java
@@ -13,5 +13,45 @@ public class Student {
     private String student_fname;
     private String student_lname;
 
+    public Student() {
+    }
 
+    public Student(long student_id, String student_fname, String student_lname) {
+        this.student_id = student_id;
+        this.student_fname = student_fname;
+        this.student_lname = student_lname;
+    }
+
+    public long getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(long student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getStudent_fname() {
+        return student_fname;
+    }
+
+    public void setStudent_fname(String student_fname) {
+        this.student_fname = student_fname;
+    }
+
+    public String getStudent_lname() {
+        return student_lname;
+    }
+
+    public void setStudent_lname(String student_lname) {
+        this.student_lname = student_lname;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "student_id=" + student_id +
+                ", student_fname='" + student_fname + '\'' +
+                ", student_lname='" + student_lname + '\'' +
+                '}';
+    }
 }
