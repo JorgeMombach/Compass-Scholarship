c5f7b4bc4e7c63d4958de035e8fa7914598f9b79
JamaykonCZ <jorge.mombach@gmail.com>
2023-08-08 15:11:27 -0300

    Added get all students mapping

--- src/main/java/jorge/mombach/school/controller/StudentController.java
+++ src/main/java/jorge/mombach/school/controller/StudentController.java
@@ -6,6 +6,8 @@ import jorge.mombach.school.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
 public class StudentController {
@@ -13,14 +15,13 @@ public class StudentController {
     @Autowired
     StudentService studentService;
 
-//    @GetMapping("/student")
-//    public StudentDtoResponse retrieveAllStudents(){
-//        return studentService.findAll();
-//    }
-
     @PostMapping("student")
-    public String post(@RequestBody StudentDtoRequest studentDtoRequest){
+    public StudentDtoRequest post(@RequestBody StudentDtoRequest studentDtoRequest){
         return studentService.save(studentDtoRequest);
     }
 
+    @GetMapping("/student")
+    public List<StudentDtoResponse> retrieveAllStudents(){
+        return studentService.findAll();
+    }
 }
--- src/main/java/jorge/mombach/school/service/StudentService.java
+++ src/main/java/jorge/mombach/school/service/StudentService.java
@@ -7,6 +7,9 @@ import jorge.mombach.school.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class StudentService {
@@ -15,18 +18,29 @@ public class StudentService {
     @Autowired
     StudentRepository studentRepository;
 
-    public String save(StudentDtoRequest studentDtoRequest){
+    public StudentDtoRequest save(StudentDtoRequest studentDtoRequest){
         Student student = new Student(
                 null,
                 studentDtoRequest.getStudent_fname(),
                 studentDtoRequest.getStudent_lname());
 
         studentRepository.save(student);
-        return "Student added";
+        return studentDtoRequest;
     }
 
-//    public StudentDtoResponse findAll() {
-//        studentRepository.findAll();
-//        return (StudentDtoResponse) students;
-//    }
+    public List<StudentDtoResponse> findAll() {
+        List<Student> students = studentRepository.findAll();
+        return students.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    private StudentDtoResponse convertToDto(Student student) {
+        return new StudentDtoResponse(
+                student.getStudent_id(),
+                student.getStudent_fname(),
+                student.getStudent_lname()
+        );
+    }
+
 }
