fb802d11e2e991d9f87f4a7b2c2309aa22dc902b
JamaykonCZ <jorge.mombach@gmail.com>
2023-08-16 17:11:24 -0300

    Fixed code and added name validation

---
+++ src/main/java/jorge/mombach/school/exception/MissingStudentNameException.java
@@ -0,0 +1,11 @@
+package jorge.mombach.school.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class MissingStudentNameException extends RuntimeException {
+    public MissingStudentNameException(String msg) {
+        super(msg);
+    }
+}
